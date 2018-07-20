38579267db7651a14d53e5c1f9d5c126ff650967
Matthew Candelaria <matthewcandelaria14@gmail.com>
2018-07-20 16:09:01 +0000

    Generated Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KingAloeCo::Application.config.secret_token = '66792ac0c89df6d780cfe7aecf7756c6f35c472a7ae6a41ab42c17f6e9cc397a308ea3d89a0211aae5f7f5674a7ba402425466d0a249e77fdc6cbeff629f53e7'
